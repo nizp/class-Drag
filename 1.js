b7ad32e62843f132b195040c0014c0a0ec4e8755
nizp <nzp890524@163.com>
2016-06-21 15:31:09 +0800

    move

--- 1.js
+++ 1.js
@@ -10,8 +10,18 @@ function Drag(id){
 
 Drag.prototype.fnDown = function(ev){
 
-	this.disX = ev.clientX - this.obj.offsetLeft;
-	this.disY = ev.clientY - this.obj.offsetTop;
+	this.disX = ev.pageX - this.obj.offsetLeft;
+	this.disY = ev.pageY - this.obj.offsetTop;
 	
+	document.addEventListener('mousemove',move);
+	
+	function move(ev){
+		
+	}
+	
+}
+Drag.prototype.fnMove = function(ev){
+	this.obj.style.left = ev.pageX - this.disX + 'px';
+	this.obj.style.top = ev.pageY - this.disY + 'px';
 }
 
