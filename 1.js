3868655b385e3041e38f87935e6ab3b9be75e920
nizp <nzp890524@163.com>
2016-06-21 11:33:58 +0800

    down

--- 1.js
+++ 1.js
@@ -1,8 +1,13 @@
-window.onload = function(){
-	
-	function Drag(id){
-		this.disX = 0;
-		this.disY = 0;
-	}
-	
+function Drag(id){
+	this.obj = document.getElementById(id);
+	this.disX = 0;
+	this.disY = 0;
+	var _this = this;
+	this.obj.addEventListener('mousedown',function(ev){
+		_this.fnDown(ev);
+	});
+}
+
+Drag.prototype.fnDown = function(ev){
+	alert(1);
 }
